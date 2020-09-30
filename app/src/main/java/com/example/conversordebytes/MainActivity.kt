62fa026cd88ca8831aee3f817df324e958484f90
aarenzanac <aarenzana@gmail.com>
2020-09-30 18:08:20 +0200

    Mas cosas en el layout

--- app/src/main/java/com/example/conversordebytes/MainActivity.kt
+++ app/src/main/java/com/example/conversordebytes/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.conversordebytes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spinnerUnidadesOrigen.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //unidadOrigen = adapterView
+            }
+        }
     }
 }
