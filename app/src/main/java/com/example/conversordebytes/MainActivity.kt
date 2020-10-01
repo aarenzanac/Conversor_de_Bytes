84c35645a310621b1108d1c8d57cb390959ec455
alex <aarenzana@gmail.com>
2020-10-01 07:03:13 +0200

    Continuo con la interfaz gráfica

--- app/src/main/java/com/example/conversordebytes/MainActivity.kt
+++ app/src/main/java/com/example/conversordebytes/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var cantidad: Double = 0.00;
-    var origen: Int = 0
-    var destino: Int = 0
+    var origen: Int = -1
+    var destino: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
                 if(origen != 0){
                     obtenerUnidadDestino()
                     if(destino != 0){
-                        //CALCULAR
+                        calcular(cantidad, origen, destino)
                     }
                 }
             }
@@ -31,22 +32,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun obtenerCantidad(){
-        if(textInputCantidad.text.toString() == null || textInputCantidad.text.toString().toDouble() == 0.00 ){
+        if(textInputCantidad.text.isNullOrBlank() || textInputCantidad.text.toString().toDouble() == 0.00 ){
             textViewResultado.setText("Debe introducir una cantidad a convertir.")
         }else{
             cantidad = textInputCantidad.text.toString().toDouble()
+            textViewResultado.setText("")
         }
     }
 
     fun obtenerUnidadOrigen(){
         spinnerUnidadesOrigen.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected (parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (parent?.getItemIdAtPosition(position) == 0.toLong()){
+                if (parent!!.getItemIdAtPosition(position).toLong().toInt() == 0){
                     textViewResultado.setText("Debe seleccionar una unidad de origen.")
-                    println("Debe seleccionar una unidad de origen.")
                 }else{
                     Toast.makeText(this@MainActivity, "Seleccionado: ${parent?.getItemAtPosition(position).toString()}", Toast.LENGTH_LONG).show()
                     origen = position
+                    textViewResultado.setText("")
                 }
             }
 
@@ -62,10 +64,10 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 if (parent?.getItemIdAtPosition(position) == 0.toLong()) {
                     textViewResultado.setText("Debe seleccionar una unidad de destino.")
-                    println("Debe seleccionar una unidad de destino.")
                 } else {
                     Toast.makeText(this@MainActivity, "Seleccionado: ${parent?.getItemAtPosition(position).toString()}", Toast.LENGTH_LONG).show()
                     destino = position
+                    textViewResultado.setText("")
                 }
             }
 
@@ -73,5 +75,11 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
+
+    fun calcular(cantidad: Double, origen: Int, destino: Int){
+
+            var resultado: Double = cantidad * 2
+            textViewResultado.setText("El resultado de la conversión es: ${resultado}")
     }
 }
